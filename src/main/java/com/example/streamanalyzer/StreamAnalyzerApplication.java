302fa0b327b686d59d724323118f442017345fd2
matvey_elovskikh <mr1504dj@mail.ru>
2025-12-14 19:02:19 +0500

    init

---
+++ src/main/java/com/example/streamanalyzer/StreamAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/streamanalyzer/StreamAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
