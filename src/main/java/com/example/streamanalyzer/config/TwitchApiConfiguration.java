f098649cbebd785a10573aa0beed0776d2b6b71c
matvey_elovskikh <mr1504dj@mail.ru>
2025-12-16 20:32:53 +0500

    refactor twitch integration

--- src/main/java/com/example/streamanalyzer/config/TwitchApiConfiguration.java
+++ src/main/java/com/example/streamanalyzer/config/TwitchApiConfiguration.java
@@ -4,36 +4,28 @@ import com.example.streamanalyzer.config.properties.TwitchApiProperties;
 import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
-import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Конфигурация Twitch4j
+ */
 @Configuration
-@RequiredArgsConstructor
 public class TwitchApiConfiguration {
-    private final TwitchApiProperties properties;
 
+    /**
+     * Бин конфигурации twitch4j
+     * @param properties параметры для клиента твич
+     * @return клиент твич
+     */
     @Bean(destroyMethod = "close")
-    public TwitchClient twitchClient() {
+    public TwitchClient twitchClient(TwitchApiProperties properties) {
         OAuth2Credential credential = new OAuth2Credential(properties.getBotUsername(),
                 properties.getAccessToken());
 
-        TwitchClient client = TwitchClientBuilder.builder()
+        return TwitchClientBuilder.builder()
                 .withChatAccount(credential)
                 .withEnableChat(true)
                 .build();
-
-        client.getEventManager().onEvent(ChannelMessageEvent.class, event -> {
-            String channel = event.getChannel().getName();
-            String username = event.getUser().getName();
-            String message = event.getMessage();
-            long timestamp = System.currentTimeMillis();
-
-            System.out.printf("[%d] #%s | %s: %s%n", timestamp, channel, username, message);
-        });
-
-        client.getChat().joinChannel(properties.getChannel());
-        return client;
     }
 }
---
+++ src/main/java/com/example/streamanalyzer/listeners/TwitchChatEventListener.java
@@ -0,0 +1,38 @@
+package com.example.streamanalyzer.listeners;
+
+import com.github.twitch4j.TwitchClient;
+import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Слушатель событий twitch чата
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TwitchChatEventListener {
+
+    private final TwitchClient client;
+
+    /**
+     * Прослушка события запуска приложения, для логирования чата у стримера
+     * @param event событие запуска приложения
+     */
+    @EventListener
+    public void onApplicationReady(ApplicationReadyEvent event) {
+        client.getEventManager().onEvent(ChannelMessageEvent.class, chatEvent -> {
+            String channel = chatEvent.getChannel().getName();
+            String username = chatEvent.getUser().getName();
+            String message = chatEvent.getMessage();
+            long timestamp = System.currentTimeMillis();
+
+            log.info("[{} ms] #{} | {}: {}", timestamp, channel, username, message);
+        });
+
+        client.getChat().joinChannel("rostikfacekid");
+    }
+}
